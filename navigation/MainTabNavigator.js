583da5999810b06ff8d8289108b65dbba7793366
William Wang <williamwang@Williams-MacBook-Pro.local>
2019-02-03 17:57:41 -0800

    change tab to Feed instead of Link

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -30,7 +30,7 @@ const LinksStack = createStackNavigator({
 });
 
 LinksStack.navigationOptions = {
-  tabBarLabel: 'Links',
+  tabBarLabel: 'Feed',
   tabBarIcon: ({ focused }) => (
     <TabBarIcon
       focused={focused}
